c15bdce03b5cec9b8c02059dbd9d66c02db18a0e
Stefaniia Vakulenko <stefany.vak@gmail.com>
2017-08-07 17:43:55 +0300

    Task10 draft

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import task9.Task9;
+
 public class Main {
     public static void main(String[] args) {
-        Task2 task2 = new Task2();
-        task2.changePublicToPrivate();
+
+        Task9.deleteAllComments();
     }
 
 }
--- src/Task10.java
+++ src/Task10.java
@@ -2,6 +2,9 @@
 
 import helpers.FileDataToStringMapper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task10 {
     private static final String PATH = "src/Task10.java";
 
@@ -13,16 +16,41 @@ public class Task10 {
     public static void switchFirstAndLastWordInLine(){
 
         String result = FileDataToStringMapper.map(PATH);
-        //if (result == null || result.isEmpty()) {
-        //    System.err.println("File contains no data.");
-        //    return;
-        //}
+        if (result == null || result.isEmpty()) {
+            System.err.println("File contains no data.");
+            return;
+        }
         System.out.println(result);
         String[] strings = result.split("\n");
 
-        for(String s : strings)
-            System.out.println(s);
 
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁa-zA-Z]+");
+
+
+        Matcher matcher;
+        String first = "";
+        String last = "";
+
+        StringBuffer buffer = new StringBuffer();
+
+        for(String s : strings) {
+            matcher = pattern.matcher(s);
+            if (matcher.find()) {
+            first = matcher.group();}
+            matcher.reset(s);
+            while (matcher.find()) {last = matcher.group();}
+            s = s.replace(first, "THISSHOULDBELAST");
+            s = s.replace(last, first);
+            s = s.replace("THISSHOULDBELAST", last);
+            buffer.append(s);
+            buffer.append("\n");
+            matcher.reset();
         }
+
+        System.out.println(buffer);
+
+
+
+    }
 
 }
